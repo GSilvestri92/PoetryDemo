f931127a8f8959f75e49d89a39fe30df0a25b7fd
Giulio Silvestri <giulio.silvestri.nttdata@skytv.it>
2024-05-03 11:42:54 +0200

    add logging to gcbq_explorer

--- gcbq_explorer/main.py
+++ gcbq_explorer/main.py
@@ -1,6 +1,8 @@
 from bq_client.bigquery_table import BigQueryTable
 from bq_client.bigquery_dataset import BigQueryDataset
-
+import sys
+import logging as log
+import click
 
 def table_exists(table_id):
     table=BigQueryTable(table_id)
@@ -21,9 +23,19 @@ def dataset_exists(dataset_id):
 @click.option('--verbosity',
               default='INFO',
               help='Set logging level')
-def print_table_properties(table_id):
+def print_table_properties(table_id,verbosity):
+
+    log.basicConfig(
+    format='%(asctime)s -- %(levelname)s:%(message)s',
+    level=str(verbosity).upper(),
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stdout)
+
+    log.info("Requesting table '{}' ".format(table_id))
     table=table_exists(table_id)
-    if table is not None:
+    log.debug("Result is object '{}' ".format(table))
+
+    if table is None:
         print("Table does not exist")
     else:
         print("Project: {}".format(table.get_project_name()))
@@ -40,9 +52,19 @@ def print_table_properties(table_id):
 @click.option('--verbosity',
               default='INFO',
               help='Set logging level')
-def print_dataset_properties(dataset_id):
+def print_dataset_properties(dataset_id,verbosity):
+
+    log.basicConfig(
+    format='%(asctime)s -- %(levelname)s:%(message)s',
+    level=str(verbosity).upper(),
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stdout)
+
+    log.info("Requesting dataset '{}' ".format(dataset_id))
     dataset=table_exists(dataset_id)
-    if dataset is not None:
+    log.debug("Result is object '{}' ".format(dataset))
+
+    if dataset is None:
         print("Dataset does not exist")
     else:
         print("Project: {}".format(dataset.get_project_name()))
